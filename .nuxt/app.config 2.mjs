90104842fef94db557a2c8e77c1417792b3f44c5
benthewill <benthewill@gmail.com>
2022-11-09 13:28:09 -0800

    Added graphql runtime.

---
+++ .nuxt/app.config 2.mjs
@@ -0,0 +1,8 @@
+
+import { defuFn } from '/Users/benthewill/Library/Mobile Documents/com~apple~CloudDocs/Indewgo/indewgo-management-app/node_modules/defu/dist/defu.mjs'
+
+const inlineConfig = {}
+
+
+
+export default defuFn(inlineConfig)
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,24 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    modules: [
+        '@nuxt/content',
+        'nuxt-graphql-client'
+    ],
+    runtimeConfig: {
+        public: {
+            'graphql-client': {
+                clients: {
+                    default: {
+                        host: 'https://rlwhlhzwqjpgcskfmeik.supabase.co/graphql/v1',
+                        headers: {
+                            apiKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJsd2hsaHp3cWpwZ2Nza2ZtZWlrIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY2MTIwMjk2MywiZXhwIjoxOTc2Nzc4OTYzfQ.3ogB3rHmLDlbWL7lkTzlcRrxtDzy7AIcjxfmwhg-pw8",
+                            authorization: "Bearer: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJsd2hsaHp3cWpwZ2Nza2ZtZWlrIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY2MTIwMjk2MywiZXhwIjoxOTc2Nzc4OTYzfQ.3ogB3rHmLDlbWL7lkTzlcRrxtDzy7AIcjxfmwhg-pw8"
+                        }
+                    }
+                }
+            }
+        }
+    },
     css: [
         '~/assets/stylesheets/main.sass'
     ]
