d125f0fc897a23a784788e5a89f2c540a1859d05
benthewill <benthewill@gmail.com>
2022-11-09 13:08:01 -0800

    Added dependencies. Checking if pages still register.

--- .nuxt/dist/client/_nuxt/index.48c9d666.js
+++
@@ -1,1 +0,0 @@
-import{a as e,b as s,o as t,e as n}from"./entry.4e5ac309.js";const o={},c=n("p",null,"This is Router Test. I'm an Index Page",-1),a=[c];function r(_,d){return t(),s("div",null,a)}const l=e(o,[["render",r]]);export{l as default};
